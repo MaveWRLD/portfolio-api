71a448a69734b47b9f6dcac8aa03d275ec069aa3
Jacob Quaye <kofimave@gmail.com>
2026-09-04 15:27:14 +0000

    feat: add django-debug-toolbar for enhanced debugging support

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -8,14 +8,11 @@ CSRF_TRUSTED_ORIGINS = ["http://localhost:5173", "http://localhost:3000", "http:
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-# debug_toolbar disabled locally: its SQL/Templates panels import
-# django.contrib.gis.gdal, which isn't installed in this env and makes every
-# server start slow (or fail) trying to locate a GDAL library.
-# INSTALLED_APPS += ["debug_toolbar"]
-# MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
-#
-# INTERNAL_IPS = ["127.0.0.1"]
-#
-# DEBUG_TOOLBAR_CONFIG = {
-#     "SHOW_TOOLBAR_CALLBACK": lambda request: True,
-# }
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": lambda request: True,
+}
